1374592bb00ef56947f7d288957a18f248137c71
howmuchadollarcost <muluyonas@yahoo.com>
2021-04-09 13:51:44 +0200

    removed router dom and fixed webpack config of image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-} from 'react-router-dom';
-
 import HomePage from './pages/HomePage/index';
-import NotFound from './pages/NotFound/index';
-import Projects from './pages/Projects/index';
 
 function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route path="/" component={HomePage} />
-				<Route exact path="/projects" component={Projects} />
-				<Route path="*" component={NotFound} />
-			</Switch>
-		</Router>
+		<>
+			<HomePage />
+		</>
 	);
 }
 
