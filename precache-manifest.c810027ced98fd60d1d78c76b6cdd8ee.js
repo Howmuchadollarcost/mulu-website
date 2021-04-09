1b1936266ed98d54f670803ad1a1ef272b8f6881
howmuchadollarcost <muluyonas@yahoo.com>
2021-04-09 08:44:54 +0200

    Updates

---
+++ precache-manifest.c810027ced98fd60d1d78c76b6cdd8ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c5a3c5446b14c7f03d9654496c71f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "362da3e476424bf7a49b",
+    "url": "/static/css/main.ac150d0a.chunk.css"
+  },
+  {
+    "revision": "9a5332a79499af98b7b5",
+    "url": "/static/js/2.f81240d8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f81240d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362da3e476424bf7a49b",
+    "url": "/static/js/main.25e6a1e9.chunk.js"
+  },
+  {
+    "revision": "4c27db2a9fd69c60cd84",
+    "url": "/static/js/runtime-main.dc4afbf8.js"
+  },
+  {
+    "revision": "28de32f7ae7e359e5f6d8c9c2b6e1438",
+    "url": "/static/media/Test.28de32f7.svg"
+  },
+  {
+    "revision": "36fe38b79260530934752aa3433c4b3e",
+    "url": "/static/media/reddit1.36fe38b7.png"
+  },
+  {
+    "revision": "17822baf7a91f0303531a82b72b29dbc",
+    "url": "/static/media/test.17822baf.png"
+  }
+]);
